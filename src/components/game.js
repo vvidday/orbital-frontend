21b1c8f0a1669a18c0db46f352f4a589e93c2dad
Guo Wei <wguo8698@gmail.com>
2022-05-26 16:51:58 +0800

    basic button and game logic with score tracking

--- src/components/game.js
+++ src/components/game.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 import { useState } from "react";
-import { setChoices } from './setChoices';
+import { setChoices } from '../logic/setChoices';
 import { getUserByUsername } from "../api/twitter";
 import { getTimeline } from "../api/twitter";
-import {data} from "./sampleData";
-
+import {data} from "../logic/sampleData";
+import { buttonLogic } from '../logic/button';
+import { score } from './score';
 
 export const Game = (
     // Twitter accounts selected by the player is passed in as props (hardcode for now)
@@ -12,6 +13,8 @@ export const Game = (
     ) => {
         const [result, setResult] = useState({});
         const [post, setPost] = useState({});
+        const [choices, allChoices] = useState([]);
+        const [reload, setReload] = useState();
         /*
             Game logic -> Depends how we can retrieve tweets. Tentatively:
                 Each Round:
@@ -27,19 +30,31 @@ export const Game = (
         /*
             Game logic(Status):
                 WIP:
-                    - Display that tweet, and write some logic for the buttons 
-                        - Correct = repeat from top (new round), score +1, store tweet somewhere so we don't dupe
+                    - Correct = store tweet somewhere so we don't dupe
                             - How to store? Tweet IDs in hash table? <- is this possible to get?
-                        - Wrong = end round, reset state, go to highscores page (KIV)
+                    - Wrong = go to highscores page (KIV)
                 DONE:
                     - Randomly select an account
                     - Randomly get a tweet from that account
                         - Only gets past 10 tweets without retweet or replies
+                    - Display that tweet, and write some logic for the buttons 
+                        - Correct = repeat from top (new round), score +1
+                        - Wrong = end round, reset state,
         */
-
-        const index = Math.floor(Math.random() * accounts.length);
-        const randomAccount = accounts[index].username;
         
+        
+        /* 
+            Uncomment for full implementation.
+            Commented out to prevent reaching tweet api request limit
+        */
+        //const index = Math.floor(Math.random() * accounts.length);
+        //const randomAccount = accounts[index].username;
+        
+        /* 
+            Comment out for full implementation.
+            Testing code below
+        */
+        const index = 1;
         useEffect(() => {
             async function userInfo() {
                 try {
@@ -49,7 +64,11 @@ export const Game = (
                     */
                     //const response = await getUserByUsername(randomAccount);
                     //setResult(response.data);
-
+                    
+                    /* 
+                        Comment out for full implementation.
+                        Testing code below
+                    */
                     setResult({id:"813286",name:"Barack Obama",username:"BarackObama"});
                 } catch (error) {
                     console.log(error);
@@ -64,7 +83,13 @@ export const Game = (
                     //const response = await getTimeline(result.id, true, true);
                     //const recentPosts = response.data;
                     //console.log(recentPosts);
+                    //const randomRecentPost = recentPosts.data[Math.floor(Math.random() * recentPosts.data.length)];
+                    //setPost(randomRecentPost.text);
 
+                    /* 
+                        Comment out for full implementation.
+                        Testing code below
+                    */
                     const recentPosts = data;
                     const randomRecentPost = recentPosts.data[Math.floor(Math.random() * recentPosts.data.length)];
                     setPost(randomRecentPost.text);
@@ -73,17 +98,24 @@ export const Game = (
                 }
             }
             userInfo().then(postInfo());
-        },[]);
+            allChoices(setChoices(index, accounts));
+        },[reload]);
     return (
         <div>
-            <div className="tweet">Sample Tweet</div>
-            <div className="tweet-data">{JSON.stringify(post).replace(/^"(.*)"$/, '$1')}</div>
-            <div>{randomAccount.username}</div>
+            <div>Score: {score}</div>
+            {
+                // Code for testing purpose
+                <div> Answer: {result.name}</div>
+            }
+            <div className="tweet">{JSON.stringify(post).replace(/^"(.*)"$/, '$1')}</div>
             <div className="options">
                 { // Reformat once we determine how accounts are stored
-                setChoices(index, result, accounts).map((acc, key) => {
-                    return <button className="option" key={key} onClick={(e)=>{}}>{acc.username}</button>
-                })};
+                choices.map((acc, key) => {
+                    return <button className="option" key={key} onClick={(e)=>{
+                        setReload(!reload);
+                        buttonLogic(result,e,accounts); 
+                    }}>{acc.name}</button>
+                })}
             </div>
         </div>
     )
---
+++ src/logic/button.js
@@ -0,0 +1,11 @@
+import { setDefault } from "../components/score";
+import { incrementScore } from "../components/score";
+
+export function buttonLogic(answer, choice, accounts) {
+    const buttonValue = choice.target.innerHTML;
+    if (buttonValue == answer.name) {
+        incrementScore();
+    } else {
+        setDefault();
+    }
+};
