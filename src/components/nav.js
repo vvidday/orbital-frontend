2f898ac859907bbea846d5e3a886036c984db8bf
vvidday <davids89034@gmail.com>
2022-05-29 15:39:29 +0800

    installed chakra-ui and styled navbar with chakra

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,23 +1,49 @@
 import React from "react";
+import {
+    Box,
+    Flex,
+    Heading,
+    Text,
+    FormControl,
+    FormLabel,
+    Switch,
+} from "@chakra-ui/react";
 
 export const Nav = ({ testing, setTesting }) => {
     return (
-        <nav id="navbar">
-            <div className="title">Who Tweeted That?</div>
-            <div>
-                <label htmlFor="testing">
-                    Testing Mode (ON = dummy data, OFF = query API){" "}
-                </label>
-                <input
-                    type="checkbox"
-                    id="testing"
-                    defaultChecked
-                    value={testing}
-                    onChange={() => {
-                        setTesting(!testing);
-                    }}
-                ></input>
-            </div>
-        </nav>
+        <Flex
+            bg="#0c0c0fff"
+            h="100px"
+            color="#ffffffff"
+            padding="10px 30px"
+            align="center"
+        >
+            <Flex as="nav" id="navbar" basis="100%" justify="space-between">
+                <Heading as="h2">
+                    Who{" "}
+                    <Text as="span" color="#00acee">
+                        Tweeted
+                    </Text>{" "}
+                    That?
+                </Heading>
+
+                <Box>
+                    <FormControl>
+                        <FormLabel htmlFor="testing">
+                            Testing Mode (ON = dummy data, OFF = query API){" "}
+                        </FormLabel>
+                        <Switch
+                            type="checkbox"
+                            id="testing"
+                            defaultChecked
+                            value={testing}
+                            onChange={() => {
+                                setTesting(!testing);
+                            }}
+                        ></Switch>
+                    </FormControl>
+                </Box>
+            </Flex>
+        </Flex>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // React Strictmode causes components to render twice - temporarily disabling.
-  //<React.StrictMode>
-    <App />
-  //</React.StrictMode>
+    // React Strictmode causes components to render twice - temporarily disabling.
+    //<React.StrictMode>
+    <ChakraProvider>
+        <App />
+    </ChakraProvider>
+    //</React.StrictMode>
 );
-
