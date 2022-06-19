11514fda31259ef1a4bfb81fc480bfaaa25cbcc1
vvidday <davids89034@gmail.com>
2022-06-19 18:45:02 +0800

    Changed choices to support less than 4 (2-3) accounts

--- src/logic/setChoices.js
+++ src/logic/setChoices.js
@@ -9,8 +9,9 @@ export function setChoices(index, accounts) {
     */
     const accountsCopy = [...accounts];
     const accountArray = [accountsCopy[index]];
+    const min = Math.min(4, accounts.length);
     accountsCopy.splice(index, 1);
-    while (accountArray.length < 4) {
+    while (accountArray.length < min) {
         const randomIndex = Math.floor(Math.random() * accountsCopy.length);
         accountArray.push(accountsCopy[randomIndex]);
         accountsCopy.splice(randomIndex, 1);
