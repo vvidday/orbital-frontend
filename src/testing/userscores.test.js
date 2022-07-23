a012250105925119bfa208f0405808df5922491d
vvidday <davids89034@gmail.com>
2022-07-23 17:34:22 +0800

    userinfo and userscores unit tests

---
+++ src/testing/userscores.test.js
@@ -0,0 +1,36 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import "../mocks/matchMedia";
+import "@testing-library/jest-dom";
+import { UserScoresImproved } from "../components/userScoresImproved";
+import mockSession from "../data/mockSession";
+
+const newMockSession = { ...mockSession };
+// Test uses fastcat821's id, to check if correct data is displayed.
+newMockSession["user"]["id"] = "3d25cd31-de0b-47b2-95c8-88509fa7376b";
+
+test("renders without crashing", () => {
+    const div = document.createElement("div");
+    render(
+        <UserScoresImproved
+            session={mockSession}
+            setGameState={null}
+            setAccs={null}
+        />,
+        div
+    );
+});
+
+test("Correctly renders group information based on session", async () => {
+    render(
+        <UserScoresImproved
+            session={newMockSession}
+            setGameState={null}
+            setAccs={null}
+        />
+    );
+    await waitFor(() => screen.findByText("Group"), { timeout: 5000 });
+    expect(screen.getByText("Group")).toBeInTheDocument();
+    expect(screen.getByText("@justinbieber")).toBeInTheDocument();
+    expect(screen.getAllByText("Play").length).toBeGreaterThan(0);
+    expect(screen.getAllByText("Share").length).toBeGreaterThan(0);
+});
